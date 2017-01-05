948c06b256f4fa030ca87cd9e364f6225d8fd689
Sam Willcocks <sam@wlcx.cc>
2017-01-05 15:09:41 +0000

    Add Ql driver
    Adds a driver for the embedded Ql database (https://github.com/cznic/ql)

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	_ "github.com/mattes/migrate/driver/postgres"
 	_ "github.com/mattes/migrate/driver/ql"
 	_ "github.com/mattes/migrate/driver/sqlite3"
+	_ "github.com/mattes/migrate/driver/ql"
 	"github.com/mattes/migrate/file"
 	"github.com/mattes/migrate/migrate"
 	"github.com/mattes/migrate/migrate/direction"
